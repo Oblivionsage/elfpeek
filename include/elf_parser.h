f023fb31f251a9fc060f146d578d96c6ea609a6a
Oblivionsage <cookieandcream560@gmail.com>
2025-12-06 14:08:04 +0100

    add elf parser header

---
+++ include/elf_parser.h
@@ -0,0 +1,19 @@
+#ifndef ELF_PARSER_H
+#define ELF_PARSER_H
+
+#include <elf.h>
+#include <stdint.h>
+
+typedef struct {
+    Elf64_Ehdr ehdr;
+    Elf64_Shdr *sections;
+    char *shstrtab;
+    uint16_t shnum;
+} ElfFile;
+
+int elf_parse_file(const char *path, ElfFile *out);
+void elf_print_header(const ElfFile *elf);
+void elf_print_sections(const ElfFile *elf);
+void elf_free(ElfFile *elf);
+
+#endif

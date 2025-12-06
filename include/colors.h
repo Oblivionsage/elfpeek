ab8e1ab79af25407c09caf842dfac8a4575f9353
Oblivionsage <cookieandcream560@gmail.com>
2025-12-06 14:07:06 +0100

    add ansi color macros

---
+++ include/colors.h
@@ -0,0 +1,14 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define CLR_RED   "\x1b[31m"
+#define CLR_GRN   "\x1b[32m"
+#define CLR_YEL   "\x1b[33m"
+#define CLR_CYN   "\x1b[36m"
+#define CLR_RST   "\x1b[0m"
+
+extern int use_colors;
+
+#define COL(c) (use_colors ? (c) : "")
+
+#endif
